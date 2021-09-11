284a667eb61e33df06d7ee6ad8eeb58404e06682
Azusa <akiriin0@gmail.com>
2021-09-11 19:44:12 -0400

    Asset Pipeline Update

---
+++ PNGSprite.cpp
@@ -0,0 +1,89 @@
+#include "PNGSprite.hpp"
+
+void PNGSprite::Fill_color_pallete(PPU466& ppu) {
+	uint8_t pallete_index = 0;
+	PPU466::Palette& pallete = ppu.palette_table[color_pallete_index];
+	for (int i=0; i < pic.size(); i++) {
+		bool in_pallete = false;
+		for (int j=0; j <= pallete_index; j++) {
+			if (pic[i] == pallete[j]) {
+				in_pallete = true;
+				break;
+			}
+		}
+		if (!in_pallete)
+			pallete[pallete_index] = pic[i];
+
+		// Max size of pallete is 4
+		if (pallete_index > 4)
+			return;
+	}
+}
+
+void PNGSprite::Register_PNG(PPU466& ppu, std::array<PPU466::Tile, 16 * 16>& tile_table, uint8_t priority) {
+	PPU466::Palette& pallete = ppu.palette_table[color_pallete_index];
+	
+	// Num of tiles for each png col/row
+	uint8_t width = PNG_SIZE / 8;
+	uint8_t height = PNG_SIZE / 8;
+
+	// For each png file
+	for (uint8_t i=0; i<width; i++){ 
+		for (uint8_t j=0; j<height; j++) {
+
+			// For each 8x8 tile
+			uint8_t curr_table_index = tile_table_index + i * width + j;
+			PPU466::Tile& tile = tile_table[curr_table_index];
+			Fill_Tile(pallete, tile, i, j);
+			Fill_Sprite(curr_table_index, priority);
+		}
+	}
+}
+
+// Inspired by Oscar's Idea of only store and retrieve 4 colors, otherwise use the first one
+uint8_t PNGSprite::Find_color(const PPU466::Palette& pallete, const glm::u8vec4& color) {
+	for (int i=0; i<pallete.size(); i++) { 
+		if (pallete[i] == color)
+			return i;
+	}
+	return 0;
+}
+
+void PNGSprite::Fill_Tile(const PPU466::Palette& pallete, PPU466::Tile& tile, uint8_t row, uint8_t col) {
+	for (uint8_t x = 0; x < 8; x++) {
+		for (uint8_t y = 0; y < 8; y++) {
+			int pic_index = (row * 8 + x) * PNG_SIZE + (col * 8 + y);
+			uint8_t color_index = Find_color(pallete, pic[pic_index]);
+			Set_Tile_Bit(tile, x, y, color_index);
+		}
+	}
+}
+
+void PNGSprite::Fill_Sprite(uint8_t t_index, uint8_t prority) {
+	PPU466::Sprite sprite;
+
+	sprite.index = t_index;
+	
+	// Set up attribute
+	sprite.attributes = prority << 7;
+	sprite.attributes |= color_pallete_index;
+
+	// TODO:Handle Sprite position offset
+
+	assert(png_sprites_index >= (PNG_SIZE * PNG_SIZE) / (8 * 8));
+	png_sprites[png_sprites_index++] = sprite;	
+}
+
+void PNGSprite::Set_Tile_Bit(PPU466::Tile& tile, uint8_t row, uint8_t col, uint8_t color_idx) {
+	uint8_t n_bit0 = color_idx & 1;
+	uint8_t n_bit1 = (color_idx >> 1) & 1;
+
+	// Initialize
+	uint16_t clean = ~(1 << col);
+	tile.bit0[row] &= clean;
+	tile.bit1[row] &= clean;
+
+	// Set Value
+	tile.bit0[row] |= n_bit0 << col;
+	tile.bit1[row] |= n_bit1 << col;
+}
--- PNGSprite.hpp
+++ PNGSprite.hpp
@@ -10,12 +10,33 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <vector>
+#include "PPU466.hpp"
+
+
+#define PNG_SIZE 32
 
 class PNGSprite {
 public:
     glm::uvec2 size;
     std::vector< glm::u8vec4 > pic;
 
+    //Only load 32x32 png, which contains 16 tiles/sprites
+    std::array<PPU466::Sprite, (PNG_SIZE*PNG_SIZE) / (8 * 8)> png_sprites;
+    uint8_t png_sprites_index = 0;
+
     PNGSprite();
+    PNGSprite(uint8_t c_idx, uint8_t t_idx) : color_pallete_index(c_idx), tile_table_index(t_idx) {}
     ~PNGSprite();
+
+private:
+	uint8_t color_pallete_index;
+    uint8_t tile_table_index;
+
+	void Fill_color_pallete(PPU466& ppu);
+	void Register_PNG(PPU466& ppu, std::array< PPU466::Tile, 16 * 16 >& tile_table, uint8_t priority);
+    uint8_t Find_color(const PPU466::Palette& pallete, const glm::u8vec4& color);
+    void Fill_Tile(const PPU466::Palette& pallete, PPU466::Tile& tile, uint8_t row, uint8_t col);
+    void Fill_Sprite(uint8_t t_index, uint8_t prority);
+    void Set_Tile_Bit(PPU466::Tile& tile, uint8_t row, uint8_t col, uint8_t color_idx);
 };
